d3c2ace70d35f557cfac0ac3a10d29c7b4ebaa6f
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-06-19 03:14:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,8 +11,8 @@ def using_unshift(bouroughs_in_nyc, new_neighborhood)
 end
 
 def using_pop(continents)
-  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  continents.pop
+  @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  @continents.pop
 end
 
 def pop_with_args(dog_breeds)
