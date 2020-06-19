67278a2ea546eca620d349f18e0241bf9ceb85be
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-06-19 02:52:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,7 @@
 def using_push
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
   updated_array = colors_in_the_rainbow.push("violet")
+  p updated_array
 end
 
   
